0843af54e06888edfc52ca8978e3625a4c1278d1
Antoine Pavy <antoine.pavy92@gmail.com>
2022-01-13 12:40:53 +0100

    MLTrainer Scene added

--- CinemaRecognizingTheme/Assets/Script/MLTrainer.cs
+++ CinemaRecognizingTheme/Assets/Script/MLTrainer.cs
@@ -14,27 +14,42 @@ namespace ML
     
 public class MLTrainer : MonoBehaviour
 {
-    public string datasetFolder;
-    [SerializeField] [Range(0f, 1f)] private float percentOfTraining;
-    public Plane selectionPlane;
-    private Camera camera;
-    [SerializeField] private MeshRenderer pointPrefab;
+    [SerializeField] private string datasetFolder;
+    [SerializeField] [Range(0f, 1f)] private float percentOfTrainingData = 0.75f;
+    [SerializeField] private int nbTests = 10;
+    [SerializeField] private int nbIterrationsPerTest = 1000;
     
-    [SerializeField] private Material blue;
-    [SerializeField] private Material red;
 
     [Header("Import & export")] 
     [SerializeField] private InputField importField;
-    [SerializeField] private InputField exportField;
 
     private List<MeshRenderer> points;
 
     private NeuralNetwork network;
     
+    private Dataset trainDataset;
+    private Dataset testDataset;
+    int trainCount = 0;
+    int testCount = 0;
+
+    private List<float> errors;
     
+
+    private void Start()
+    {
+        network = new NeuralNetwork(false);
+        network.AddLayer(2, OutputFunction.Linear);
+        network.AddLayer(3, OutputFunction.Sigmoid);
+        network.AddLayer(1, OutputFunction.Sigmoid);
+        
+        points = new List<MeshRenderer>();
+        testDataset = new Dataset();
+        trainDataset = new Dataset();
+    }
+
     public void ImportFromCSV()
     {
-        clearPoints();
+        Clear();
         StreamReader str = new StreamReader(Application.dataPath + "/StreamingAssets/Datasets/" + datasetFolder + importField.text + ".csv");
         bool endFile = false;
         str.ReadLine();
@@ -64,14 +79,10 @@ public class MLTrainer : MonoBehaviour
         IOrderedEnumerable<Vector3> shuffledData = rawDatas.OrderBy(item => rnd.Next());
         
         
-        Dataset trainDataset = new Dataset();
-        Dataset testDataset = new Dataset();
         int i = 0;
-        int trainCount = 0;
-        int testCount = 0;
         foreach(Vector3 data in shuffledData)
         {
-            if (i < countDatas * percentOfTraining)
+            if (i < countDatas * percentOfTrainingData)
             {
                 trainDataset.AddData(new float[]{data.x, data.y}, new float[]{data.z});
                 trainCount++;
@@ -86,41 +97,30 @@ public class MLTrainer : MonoBehaviour
         }
         
         Debug.Log("Dataset separated into train and test. " + trainCount + "/" + testCount);
-        Debug.Log("Beggining of training");
-        
-        network.Train(trainDataset, 100000, 0.01f, true);
-        Debug.Log("Training finished.");
-        Debug.Log("Starting evaluation.");
-        float error = network.Evaluate(testDataset,testCount,0.1f);
-        Debug.Log("Evaluation finished. ");
-        Debug.Log("Error : " + error*100f + "%");
         
     }
-    
-    public void clearPoints()
+
+    public void TrainML()
     {
-        foreach (var point in points)
+        if (trainDataset.GetInputCount() > 0)
         {
-            Destroy(point.gameObject);
+            Debug.Log("Beggining of training");
+            for (int i = 0; i < nbTests; i++)
+            {
+                network.Train(trainDataset, nbIterrationsPerTest, 0.01f, true);
+                errors.Add(network.Evaluate(testDataset,testCount,0.1f));
+            }
+
+            Debug.Log("Training finished. Total amount of itterations : " + nbTests * nbIterrationsPerTest);
+            Debug.Log("Final error percent : " + errors[nbTests-1] *100f + "%");
         }
-        points.Clear();
+        else
+        {
+            Debug.LogError("Please import datas before training.");
+        }
     }
-
-    private void Start()
+    public void Clear()
     {
-        
-        network = new NeuralNetwork();
-        network.AddLayer(2, OutputFunction.Linear);
-        network.AddLayer(3, OutputFunction.Sigmoid);
-        network.AddLayer(1, OutputFunction.Sigmoid);
-        
-        camera = FindObjectOfType<Camera>();
-        points = new List<MeshRenderer>();
-    }
-
-    private void Update()
-    {
-       
     }
 }
 
