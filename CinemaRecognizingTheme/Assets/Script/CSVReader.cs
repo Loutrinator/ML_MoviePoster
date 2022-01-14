4aa6e3b286056f20d6154416bd0423886946c4c8
Antoine Pavy <antoine.pavy92@gmail.com>
2022-01-15 00:43:14 +0100

    Cleaned up file reading

--- CinemaRecognizingTheme/Assets/Script/CSVReader.cs
+++ CinemaRecognizingTheme/Assets/Script/CSVReader.cs
@@ -3,19 +3,14 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-public class CSVReader : MonoBehaviour
+public class CSVReader
 {
-    [SerializeField]
-    private GameObject point;
-
-    [SerializeField] private string filePath;
-
-    // Start is called before the first frame update
-    void Start()
+    public static List<string[]> Read(string filepath, char separator)
     {
-        StreamReader str = new StreamReader(Application.dataPath + "/StreamingAssets/Datasets/" + filePath);
+        StreamReader str = new StreamReader(Application.dataPath + filepath);
         bool endFile = false;
         str.ReadLine();
+        List<string[]> datas = new List<string[]>();
         while (!endFile)
         {
             string data = str.ReadLine();
@@ -24,20 +19,10 @@ public class CSVReader : MonoBehaviour
                 endFile = true;
                 break;
             }
-            var value = data.Split(';');
-            Debug.Log("Ligne : " + value[0] + "      " + value[1] + "     " + value[2]);
-            
-            Color c = point.GetComponent<SpriteRenderer>().color;
-
-            if (float.Parse(value[2]) > 0.5) c = Color.red;
-            else c = Color.blue;
-
-            point.GetComponent<SpriteRenderer>().color = c;
-
-            point.transform.position = new Vector2(float.Parse(value[0]), float.Parse(value[1]));
-
-            Instantiate(point);
+            datas.Add(data.Split(separator));
         }
+
+        return datas;
     }
 
 }
--- CinemaRecognizingTheme/Assets/Script/ImageLoader.cs
+++ CinemaRecognizingTheme/Assets/Script/ImageLoader.cs
@@ -6,24 +6,18 @@ using UnityEngine.UI;
 
 public class ImageLoader : MonoBehaviour
 {
-    [SerializeField] Text imageName;
     public int width = 0;
     public int height = 0;
 
-    public void LoadImage()
+    public Texture2D LoadImage(string imageId)
     {
-        string imagePath = Application.dataPath + "/StreamingAssets/Datasets/Movies/Images/" + imageName.text + ".jpg";
+        string imagePath = Application.dataPath + "/StreamingAssets/Datasets/Movies/Images/" + imageId + ".jpg";
 
         Texture2D tex;
         tex = new Texture2D(width, height);
         WWW ww = new WWW(imagePath);
-
-        GameObject ri = new GameObject();
-        ri.AddComponent<Image>();
         ww.LoadImageIntoTexture(tex);
-
-        ri.GetComponent<Image>().sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), Camera.current.transform.position);
-
+        return tex;
     }
 
 }
